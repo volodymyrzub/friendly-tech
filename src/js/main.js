3d0b45a917497540c80722fb0a932c7160fa162a
vovanz <volodymyr.zub@gmail.com>
2015-08-18 11:26:39 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
