1fd1d0a243ed5f87b8a5fe4a5b0cb3bb72913735
vovanz <volodymyr.zub@gmail.com>
2015-08-21 17:38:17 +0300

    (layout version 1)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
  
  $('.owl-carousel').owlCarousel({
-		autoPlay: 3000,
-		items : 4,
+ 		items: 4,
+ 		loop: false,
+		autoPlay: true,
+		autoplayTimeout: 2000,
+		touchDrag: true,
+		itemsDesktop: [1199,4],
+		itemsDesktopSmall: [979,3],
+		itemsTablet: [768,2],
+		itemsMobile: [479,1],
+		navigation: true,
+		pagination: false,
+		navigationText : ["<span class=\"glyphicon glyphicon-menu-left\" aria-hidden=\"true\"></span>", "<span class=\"glyphicon glyphicon-menu-right\" aria-hidden=\"true\"></span>"]
  	});
  
 });
